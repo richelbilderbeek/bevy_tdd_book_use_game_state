ff275120f44e463f8e8c1a819e1e12e756333d39
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-09-22 20:05:14 +0200

    EOD

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ use bevy::prelude::*;
 mod app;
 
 fn main() {
-    let text = String::from("Hello from main");
-    let mut app = create_app(text);
+    let mut app = create_app();
     let add_camera_fn = |mut commands: Commands| {
         commands.spawn(Camera2dBundle::default());
     };
     app.add_systems(Startup, add_camera_fn);
-    app.add_plugins(DefaultPlugins);
+    //app.add_plugins(DefaultPlugins);
     app.run();
 }
