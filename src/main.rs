9bfb9e3c89267827b43c2c3b1c786e3acac49423
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-26 11:24:05 +0100

    Simplify code

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod app;
 fn main() {
     let mut app = create_app();
     //let add_camera_fn = |mut commands: Commands| {
-    //    commands.spawn(Camera2d::default());
+    //    commands.spawn(Camera2d);
     //};
     //app.add_systems(Startup, add_camera_fn);
     //app.add_plugins(DefaultPlugins);
